e07c0d7b37aa3d2b779a4dd5e5ff7f8f4cc49bdb
Austine Juma <austinejumah001@gmail.com>
2026-02-02 19:04:51 +0300

    refactor: return all lowercase computer choices
    This enables easier comparison with human choices to evaluate game result.

--- index.js
+++ index.js
@@ -40,11 +40,11 @@ function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     switch(randomNumber){
         case 1: 
-            return "Rock";
+            return "rock";
         case 2:
-            return "Paper";
+            return "paper";
         case 3: 
-            return "Scissors"
+            return "scissors"
     }
 
 }
