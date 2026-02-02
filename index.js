65f03a03daa5bc08a038f1f80b1113a598b77a6c
Austine Juma <austinejumah001@gmail.com>
2026-02-02 18:37:13 +0300

    feat: add function to generate a random computer move

--- index.js
+++ index.js
@@ -35,3 +35,18 @@ PSEUDOCODE
             
 
 */
+   
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    switch(randomNumber){
+        case 1: 
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3: 
+            return "Scissors"
+    }
+
+}
+
+console.log(getComputerChoice())
